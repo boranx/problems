d6c6fc664ef8f4f48238bda7a6386936d995b49a
boranx <boran.seref@gmail.com>
2021-08-30 12:40:46 +0200

    largest product in a grid

---
+++ largest-product-in-grid/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+
+const (
+	grid = `
+			08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+			49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+			81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+			52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+			22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+			24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+			32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+			67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+			24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+			21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+			78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+			16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+			86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+			19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+			04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+			88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+			04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+			20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+			20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+			01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48`
+)
+
+func convertToMatrix(s string) [20][20]int {
+	var list [20][20]int // assumes the input is 20x20
+
+	eachRow := strings.Split(s, "\n")
+	for i := 0; i < 20; i++ {
+		subCol := strings.Fields(eachRow[i])
+		for j := 0; j < len(subCol); j++ {
+			v, _ := strconv.Atoi(subCol[j])
+			list[i][j] = v
+		}
+	}
+
+	return list
+}
+
+func calculateBiggest(list [20][20]int) int {
+	biggest := 0
+	for i := 0; i < 20; i++ {
+		for j := 0; j < 20; j++ {
+
+			var sub_biggest []int
+			// calculate down
+			if i+3 < 20 {
+				sub_biggest = append(sub_biggest, list[i][j]*list[i+1][j]*list[i+2][j]*list[i+3][j])
+			}
+
+			// calculate up
+			if i-3 >= 0 {
+				sub_biggest = append(sub_biggest, list[i][j]*list[i-1][j]*list[i-2][j]*list[i-3][j])
+			}
+
+			//calculate right
+			if j+3 < 20 {
+				sub_biggest = append(sub_biggest, list[i][j]*list[i][j+1]*list[i][j+2]*list[i][j+3])
+			}
+
+			//calculate left
+			if j-3 >= 0 {
+				sub_biggest = append(sub_biggest, list[i][j]*list[i][j-1]*list[i][j-2]*list[i][j-3])
+			}
+
+			//calculate diagonal up left
+			if i-3 >= 0 && j-3 >= 0 {
+				sub_biggest = append(sub_biggest, list[i][j]*list[i-1][j-1]*list[i-2][j-2]*list[i-3][j-3])
+			}
+
+			//calculate diagonal up right
+			if i-3 >= 0 && j+3 < 20 {
+				sub_biggest = append(sub_biggest, list[i][j]*list[i-1][j+1]*list[i-2][j+2]*list[i-3][j+3])
+			}
+
+			//calculate diagonal down left
+			if i+3 < 20 && j-3 >= 0 {
+				sub_biggest = append(sub_biggest, list[i][j]*list[i+1][j-1]*list[i+2][j-2]*list[i+3][j-3])
+			}
+
+			//calculate diagonal down right
+			if i+3 < 20 && j+3 < 20 {
+				sub_biggest = append(sub_biggest, list[i][j]*list[i+1][j+1]*list[i+2][j+2]*list[i+3][j+3])
+			}
+
+			m := max(sub_biggest)
+			if biggest < m {
+				biggest = m
+			}
+		}
+	}
+
+	return biggest
+}
+
+func max(l []int) int {
+	max := 0
+	for _, v := range l {
+		if max < v {
+			max = v
+		}
+	}
+	return max
+}
+
+func main() {
+	v := convertToMatrix(grid)
+	fmt.Println(calculateBiggest(v)) // 70600674
+}
